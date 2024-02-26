431e91c26a4f26d123f97bba3bdbf2f5eb384fb1
tylguo <tyleguo2004@gmail.com>
2024-02-26 15:40:51 -0800

    update about me

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -6,7 +6,7 @@ export const AboutMe = () => {
         <div className='Home'>
             <h1>About Me</h1>
             <p style={{color: 'indianred'}}>My name is Tyler</p>
-            <p style={{color: 'indianred'}}>I am a 2nd year Statistics Major</p>
+            <p style={{color: 'indianred'}}>I am a 2nd year statistics major</p>
         </div>
     )
 }
